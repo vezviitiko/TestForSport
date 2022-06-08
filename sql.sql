bb8f97d379e79f616ef0611958c780d1c4443ca0
Alex Comis <38458279+vezviitiko@users.noreply.github.com>
2022-06-08 23:10:00 +0800

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -4,6 +4,7 @@ select SUM(COALESCE("AMOUNT",0)) AS SUM, date::date
 from generate_series('01.01.2020', '31.01.2020', interval '1 day') as date
 	left join n_daily_rests 
 	on date::date between "DATEBEGIN" and "DATEEND" 
+	where id_obj in (171,172,174)
 	group by date
 	order by date
 
