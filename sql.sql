e54c94804a55363d13a45e8ffb0b352a8de65f89
Alex Comis <38458279+vezviitiko@users.noreply.github.com>
2022-06-28 11:51:26 +0800

    add task1

--- sql.sql
+++ sql.sql
@@ -1,11 +1,23 @@
 -- Task 1
+-- решение через представление(более понятно и проще разобраться)
 
-select SUM(COALESCE("AMOUNT",0)) AS SUM, date::date
-from generate_series('01.01.2020', '31.01.2020', interval '1 day') as date
-	left join n_daily_rests 
-	on date::date between "DATEBEGIN" and "DATEEND" 
-	where id_obj in (171,172,174)
-	group by date
-	order by date
+CREATE VIEW dat AS (select gs::date from generate_series('2020-01-01', '2020-01-31', interval '1 day') as gs);
+SELECT d.gs, COALESCE(sum(amount),0) as sm 
+	FROM dat d LEFT JOIN n_daily_rests n 
+	ON d.gs >= n.datebegin 
+	AND d.gs < n.dateend 
+	AND n.id_obj IN (171, 172, 174) 
+	GROUP BY d.gs ORDER BY d.gs;
 
+-- решение одним запросом
+
+SELECT d.gs, COALESCE(sum(amount),0) as sm FROM 
+	(select gs::date 
+	 from generate_series('2020-01-01', '2020-01-31', interval '1 day') as gs) d 
+	 LEFT JOIN n_daily_rests n 
+	 ON d.gs >= n.datebegin 
+	 AND d.gs < n.dateend 
+	 AND n.id_obj IN (171, 172, 174) 
+	 GROUP BY d.gs ORDER BY d.gs;
+	 
 -- Task 2
